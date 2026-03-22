2266b42886db02b4cffb237af426e6e01ae83cee
Rafi <dsrepair71@gmail.com>
2026-03-22 07:38:35 -0700

    Commit43-added "if"

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -1,7 +1,33 @@
 resource "random_string" "suffix" {
   length = 6
+  upper = false
+  special = false
 }
 
 locals {
   environment_prefix = "${var.application_name}-${var.environment_name}-${random_string.suffix.result}"
+}
+
+resource "random_string" "map" {
+ 
+ for_each = var.regions_instance_count
+ 
+  length = 6
+  upper = false
+  special = false
+
+}
+
+resource "random_string" "if" {
+
+  count = var.enabled ? 1 : 0
+
+  length = 6
+  upper = false
+  special = false
+}
+
+module "module" {
+source  = "hashicorp/module/random"
+version = "1.0.0"
 }
